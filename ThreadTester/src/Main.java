03b16c82d1d8102fef830be4f1d4cebed10b58d4
Ankit Bansal <ankitbansalbits@gmail.com>
2024-09-01 12:32:47 +0530

    Create thread using Runnable interface

--- ThreadTester/src/Main.java
+++ ThreadTester/src/Main.java
@@ -2,9 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("main thread starting");
+
+        // implement thread using Thread class
         Thread thread1 = new Thread1("thread1Name");
        // thread1.setDaemon(true);
         thread1.start();
+
+        // implement thread using runnable interface.
+        // we use the default constructor of THread
+        Thread thread2 = new Thread(new Thread2(), "thread2");
+        thread2.start();
+
         System.out.println("main thread exiting ");
     }
 }
